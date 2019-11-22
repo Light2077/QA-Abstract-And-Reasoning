60c04cca057fa21ce020a5fe1bf86e2965da38e7
Light <Light1912@users.noreply.github.com>
2019-11-23 00:02:47 +0800

    测试pycharm与github的连接

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # QA abstract and reasoning
+ 2019年11月22日建立
  
+ what
+ 
+ something new哒哒哒付
--- code/preprocess.py
+++ code/preprocess.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import numpy as np
+
+apple = np.array([1, 2, 3])
+print(pd.Series(apple))
+
+# what
+
