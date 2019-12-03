e836aa2a92359fe0cef4f91f9a2b0268e9428173
Light2077 <435786117@qq.com>
2019-12-03 14:51:24 +0800

    add path: WV_MODEL_PAD

--- utils/config.py
+++ utils/config.py
@@ -26,3 +26,5 @@ TEST_X_PAD = os.path.join(root, 'data', 'test_X_pad.csv')
 WV_MODEL = os.path.join(root, 'data', 'wv', 'word2vec.model')
 VOCAB_INDEX = os.path.join(root, 'data', 'wv', 'vocab_index.txt')
 EMBEDDING_MATRIX = os.path.join(root, 'data', 'wv', 'embedding_matrix.txt')
+
+WV_MODEL_PAD = os.path.join(root, 'data', 'wv', 'word2vec_pad.model')
