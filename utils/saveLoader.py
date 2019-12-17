81e32f9d40f926996167bb5bbd09445106e606d3
Light2077 <435786117@qq.com>
2019-12-17 11:31:25 +0800

    add class Vocab in saveLoader.py

--- utils/saveLoader.py
+++ utils/saveLoader.py
@@ -70,6 +70,57 @@ def load_dataset(train_data_path_, test_data_path_):
     return train_data, test_data
 
 
+class Vocab:
+    PAD_TOKEN = '<PAD>'
+    UNKNOWN_TOKEN = '<UNK>'
+    START_DECODING = '<START>'
+    STOP_DECODING = '<STOP>'
+
+    def __init__(self, vocab_file, vocab_max_size=None):
+        """
+        Vocab 对象,vocab基本操作封装
+        :param vocab_file: Vocab 存储路径
+        :param vocab_max_size: 最大字典数量
+        """
+        self.word2id, self.id2word = self.load_vocab(vocab_file, vocab_max_size)
+        self.count = len(self.word2id)
+
+    @staticmethod
+    def load_vocab(file_path, vocab_max_size=None):
+        """
+        读取字典
+        :param file_path: 文件路径
+        :return: 返回读取后的字典
+        """
+        vocab = {}
+        reverse_vocab = {}
+        for line in open(file_path, "r", encoding='utf-8').readlines():
+            word, index = line.strip().split("\t")
+            index = int(index)
+            # 如果vocab 超过了指定大小
+            # 跳出循环 截断
+            if vocab_max_size and index > vocab_max_size:
+                print("max_size of vocab was specified as %i; we now have %i words. Stopping reading." % (
+                    vocab_max_size, index))
+                break
+            vocab[word] = index
+            reverse_vocab[index] = word
+        return vocab, reverse_vocab
+
+    def word_to_id(self, word):
+        if word not in self.word2id:
+            return self.word2id[self.UNKNOWN_TOKEN]
+        return self.word2id[word]
+
+    def id_to_word(self, word_id):
+        if word_id not in self.id2word:
+            raise ValueError('Id not found in vocab: %d' % word_id)
+        return self.id2word[word_id]
+
+    def size(self):
+        return self.count
+
+
 def save_vocab(path, vocab_index):
     """
 
